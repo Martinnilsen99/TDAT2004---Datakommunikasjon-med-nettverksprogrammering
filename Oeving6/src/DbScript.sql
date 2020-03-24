db7ddec83816bdf6263f873401c57ca9152509e9
Martin Johannes Nilsen <martin@ivar-nilsen.no>
2020-03-24 12:49:15 +0100

    fix

--- Oeving6/src/DbScript.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS Konto;
-CREATE TABLE Konto(
-  kontonr integer NOT NULL,
-  saldo double,
-  eier varchar(250) NOT NULL,
-  optimistiskLås integer NOT NULL,
-  PRIMARY KEY (kontonr)
-);
